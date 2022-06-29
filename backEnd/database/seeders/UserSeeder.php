f6d9fb8f3972fa05f7dee142ff94b7e35db5a88e
vauzi <ismanvauzi1998@gmail.com>
2022-06-28 13:40:06 -1200

    user seeder

--- backEnd/database/seeders/UserSeeder.php
+++ backEnd/database/seeders/UserSeeder.php
@@ -19,31 +19,35 @@ class UserSeeder extends Seeder
             'name'  => 'Admin',
             'email' => 'admin@admin.com',
             'role'  => 'admin',
-            'password'  => 'admin123'
+            'email_verified_at' => now(),
+            'password'  => bcrypt('admin123')
         ]);
         User::create([
             'name'  => 'User',
             'email' => 'user@user.com',
             'role'  => 'user',
-            'password'  => 'user123'
+            'email_verified_at' => now(),
+            'password'  => bcrypt('user123')
         ]);
         User::create([
             'name'  => 'Isaman',
             'email' => 'isman@user.com',
             'role'  => 'admin',
-            'password'  => 'admin123'
+            'email_verified_at' => now(),
+            'password'  => bcrypt('admin123'),
         ]);
         User::create([
             'name'  => 'Vauzi',
             'email' => 'vauzi@user.com',
             'role'  => 'uer',
-            'password'  => 'user123'
+            'email_verified_at' => now(),
+            'password'  => bcrypt('user123'),
         ]);
         User::create([
             'name'  => 'Yusalam',
             'email' => 'yuslam@user.com',
             'role'  => 'user',
-            'password'  => 'user123'
+            'password'  => bcrypt('user123')
         ]);
     }
 }
